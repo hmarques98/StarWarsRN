029e6d7b6271a242ad57e7371cb270e145886b9b
Henrique Marques <marquesprogrammer@hotmail.com>
2021-05-17 09:17:36 -0300

    feat: Character Detail screen showing more info

--- src/screens/CharacterDetail.tsx
+++ src/screens/CharacterDetail.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import CustomText from 'components/CustomText';
 import { CommonStackParamList } from 'screens';
 import CustomScreen from 'components/CustomScreen';
@@ -11,11 +11,19 @@ import { myTheme } from 'theme';
 import { Typography } from 'components/molecules/Typography';
 import UserAvatar from 'react-native-user-avatar';
 import { FONT_SIZE_16 } from 'styles/typography';
-
+import { Button } from 'components/molecules/Button';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { WINDOW_DEVICE_WIDTH } from '@utils/constants';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 type ScreenRouteProp = RouteProp<CommonStackParamList, 'CharacterDetail'>;
+type CharacterDetailScreenNavigationProp = StackNavigationProp<
+  CommonStackParamList,
+  'Home'
+>;
 
-const CustomWebView = () => {
+const CharacterDetailScreen = () => {
   const { params } = useRoute<ScreenRouteProp>();
+  const { goBack } = useNavigation<CharacterDetailScreenNavigationProp>();
   const { name, ...restProperties } = params.character;
 
   return (
@@ -52,12 +60,29 @@ const CustomWebView = () => {
             Skin color - {restProperties.skin_color}
           </Typography>
         </Box>
+        <Box width={WINDOW_DEVICE_WIDTH * 0.8}>
+          <Button
+            onPress={goBack}
+            bg={myTheme.colors.secondary}
+            width="100%"
+            flexDirection="row"
+            alignItems="center">
+            <Box marginRight={2}>
+              <AntDesignIcon
+                name="arrowleft"
+                color={myTheme.colors.primary}
+                size={20}
+              />
+            </Box>
+            <Typography color={myTheme.colors.primary}>Come back</Typography>
+          </Button>
+        </Box>
       </Box>
     </SafeAreaView>
   );
 };
 
-export default CustomWebView;
+export default CharacterDetailScreen;
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
